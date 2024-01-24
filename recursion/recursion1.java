162d727dc8a17df01a2d99e22cddf77070f1c3d2
Dhanush <danush2228s@gmail.com>
2024-01-24 07:17:53 +0530

    Printed a  name n times using  recursion

--- recursion/recursion1.java
+++ recursion/recursion1.java
@@ -1,5 +1,15 @@
 public class recursion1 {
     public static void main(String[] args) {
-        System.out.println("Dhanush ");
+        printName(3);
+    }
+
+    static void printName(int n){
+        if (n==1){
+            System.out.println("Dhanush");
+            return ;
+        }
+        System.out.print("Dhanush");
+        printName(n-1);
+
     }
 }
